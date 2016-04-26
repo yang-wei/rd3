11529694b83677645adf718c8b3ff985f32efb65
yang-wei <yangweilim21@gmail.com>
2016-04-26 23:26:00 +0900

    Add animation test to make barchart-test fail

--- tests/barchart-tests.js
+++ tests/barchart-tests.js
@@ -74,4 +74,46 @@ describe('BarChart', function() {
       barchart, 'rect');
     expect(bars.length).to.equal(data.length * length);
   });
+
+  it('animates bar correctly', function() {
+
+    var BarChart = require('../src/barchart').BarChart;
+    var generate = require('./utils/datagen').generateArrayOfPoints;
+    var length = 5;
+
+    var data = [
+      {
+        name: "series1",
+        values: generate(length)
+      },
+      {
+        name: "series2",
+        values: generate(length)
+      }
+    ];
+
+    // Render a barchart using array data
+    var barchart = TestUtils.renderIntoDocument(
+      <BarChart data={data} width={400} height={200} />
+    );
+
+    // Verify that it has rendered the main chart svg
+    var bars = TestUtils.scryRenderedDOMComponentsWithTag(
+      barchart, 'rect');
+
+    // Before animation
+    var bar = bars[0];
+    var defaultFill = bar.getAttribute('fill');
+
+    // Animation starts with hover
+    TestUtils.Simulate.mouseOver(bar);
+    expect(bar.getAttribute('fill')).to.not.equal(defaultFill);
+
+    // TestUtils.Simulate.mouseOut(circle) is not working here
+    // https://github.com/facebook/react/issues/1297
+    // Animation ends with end of hover
+    TestUtils.SimulateNative.mouseOut(bar);
+    expect(bar.getAttribute('fill')).to.equal(defaultFill);
+
+  });
 });
