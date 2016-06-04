a884e600ac3556f5dd9e71aec6eeb43c38ef6163
yang-wei <yangweilim21@gmail.com>
2016-06-04 21:04:36 +0900

    Tool Tip Wrapper

---
+++ src/common/ToolTipWrapper.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var React = require('react');
+
+const _renderToolTip = function(state) {
+
+  if(!state.dataPoint) {
+    return;
+  }
+
+  const containerStyles = {
+    position: 'fixed',
+    top: state.y,
+    left: state.x,
+    display: 'inherit',
+    opacity: 0.8,
+  };
+  // TODO: add 'right: 0px' style when tooltip is off the chart
+  const tooltipStyles = {
+    position: 'absolute',
+    backgroundColor: 'white',
+    border: '1px solid',
+    borderColor: '#ddd',
+    borderRadius: '2px',
+    padding: '10px',
+    marginLeft: '10px',
+    marginRight: '10px',
+    marginTop: '-15px',
+  };
+
+  return (
+    <div style={containerStyles}>
+      <div style={tooltipStyles}>
+        {String(state.dataPoint.yValue)}
+      </div>
+    </div>
+  );
+}
+
+export function toolTipWrapper (Component, toolTip = _renderToolTip) {
+
+  return React.createClass({
+
+    getInitialState() {
+      return {
+        tooltip: {
+          x: 0,
+          y: 0,
+          dataPoint: '',
+          show: false,
+        }
+      };
+    },
+
+    onMouseOver(x, y, dataPoint) {
+      this.setState({
+        tooltip: {
+          x,
+          y,
+          dataPoint: dataPoint,
+          show: true,
+        }
+      });
+    },
+
+    onMouseLeave() {
+      this.setState({
+        tooltip: {
+          x: 0,
+          y: 0,
+          child: '',
+          show: false,
+        }
+      });
+    },
+
+    render() {
+      return (
+        <span>
+          <Component
+            {...this.props}
+            {...this.state}
+            onMouseOver={this.onMouseOver}
+            onMouseLeave={this.onMouseLeave}
+          />
+          {toolTip(this.state.tooltip)}
+        </span>
+      )
+    },
+
+  });
+
+}
